4819fe79bc1a6bb71b5f0d0ff2bbbc9c4d15cc9a
ChrisLamanu <christopher.vrel@gmail.com>
2021-01-25 15:53:19 +0100

    Adding exo part 3 + TP

--- sql_exo_part_3.sql
+++ sql_exo_part_3.sql
@@ -1,39 +1,23 @@
 -- Exo 1
-CREATE TABLE webDevelopment.languages(id INT PRIMARY KEY AUTO_INCREMENT, language VARCHAR(255));
+ALTER TABLE webDevelopment.languages ADD versions VARCHAR(255); 
 
 -- Exo 2
-CREATE TABLE webDevelopment.tools(id INT PRIMARY KEY AUTO_INCREMENT, tool VARCHAR(255));
+ALTER TABLE webDevelopment.frameworks ADD version INT;
 
 -- Exo 3
-CREATE TABLE webDevelopment.frameworks(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255));
+ALTER TABLE webDevelopment.languages CHANGE versions version VARCHAR(255);
 
 -- Exo 4
-CREATE TABLE webDevelopment.libraries(id INT PRIMARY KEY AUTO_INCREMENT, library VARCHAR(255));
+ALTER TABLE webDevelopment.frameworks CHANGE name framework VARCHAR(255);
 
 -- Exo 5
-CREATE TABLE webDevelopment.ide(id INT PRIMARY KEY AUTO_INCREMENT, ideName VARCHAR(255));
-
--- Exo 6
-CREATE TABLE IF NOT EXISTS webDevelopment.frameworks(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255));
-
--- Exo 7
-DROP TABLE IF EXISTS webDevelopment.tools;
-
--- Exo 8
-DROP TABLE webDevelopment.libraries;
-
--- Exo 9
-DROP TABLE webDevelopment.ide;
+ALTER TABLE webDevelopment.frameworks MODIFY version VARCHAR(10);
 
 -- TP
-CREATE DATABASE IF NOT EXISTS codex CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE TABLE codex.clients(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    lastname VARCHAR(255),
-    firstname VARCHAR(255),
-    birthDate DATETIME DEFAULT NOW(),
-    address VARCHAR(255),
-    firstPhoneNumber INT,
-    secondPhoneNumber INT,
-    mail VARCHAR(255)
+ALTER TABLE codex.clients DROP secondPhoneNumber;
+ALTER TABLE codex.clients CHANGE firstPhoneNumber phoneNumber INT;
+ALTER TABLE codex.clients MODIFY phoneNumber VARCHAR(255);
+ALTER TABLE codex.clients ADD (
+    zipCode VARCHAR(255),
+    city VARCHAR(255)
 );
